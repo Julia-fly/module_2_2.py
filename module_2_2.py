55eccffbff5de70e4508bd38c2f9e91af55ffb80
Julia-fly <uliasergeeva3896@gmail.com>
2024-10-28 21:20:47 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = (int(input("Введите число")))
+second = (int(input("Введите число")))
+third = (int(input("Введите число")))
+if third == first == second == third :
+    print("3")
+if third != first != second != third :
+    print ("0")
+else:
+    print ("2")
